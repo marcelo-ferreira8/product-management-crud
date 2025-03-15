bda95562f42a22e75f537730ce91cff6b6e70ef0
Marcelo Ferreira <flaviomff@gmail.com>
2025-03-15 14:48:05 -0300

    feat: products DTO

---
+++ backend/src/produtos/produtos.dto.ts
@@ -0,0 +1,11 @@
+export class CreateProdutoDto {
+  nome: string;
+  preco: number;
+  quantidade: number;
+}
+
+export class UpdateProdutoDto {
+  nome: string;
+  preco: number;
+  quantidade: number;
+}
