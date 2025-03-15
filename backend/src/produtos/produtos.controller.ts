c3e41af0312f0d483d791688437aac8cefd88a2b
Marcelo Ferreira <flaviomff@gmail.com>
2025-03-15 14:47:42 -0300

    feat: products controller & service

---
+++ backend/src/produtos/produtos.controller.ts
@@ -0,0 +1,42 @@
+// src/produtos/produtos.controller.ts
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { ProdutosService } from './produtos.service';
+import { CreateProdutoDto, UpdateProdutoDto } from './produtos.dto';
+
+@Controller('produtos')
+export class ProdutosController {
+  constructor(private readonly produtosService: ProdutosService) {}
+
+  @Post()
+  create(@Body() createProdutoDto: CreateProdutoDto) {
+    return this.produtosService.create(createProdutoDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.produtosService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.produtosService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateProdutoDto: UpdateProdutoDto) {
+    return this.produtosService.update(+id, updateProdutoDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.produtosService.remove(+id);
+  }
+}
